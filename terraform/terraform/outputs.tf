a5c0bc534ece9efd61fd85216d07e56971fcd0c5
kurasevych <andrii.kurasevych.pp.2022@lpnu.ua>
2026-04-06 18:39:26 +0300

    Update outputs.tf

--- terraform/terraform/outputs.tf
+++ terraform/terraform/outputs.tf
@@ -1,24 +1,24 @@
 output "vpc_name" {
-  description = "VPC network name"
-  value       = google_compute_network.vpc.name
+  description = "VPC name"
+  value       = digitalocean_vpc.vpc.name
 }
 
-output "vm_public_ip" {
-  description = "Public IP of the VM"
-  value       = google_compute_instance.node.network_interface[0].access_config[0].nat_ip
+output "droplet_ip" {
+  description = "Публічна IP-адреса VM"
+  value       = digitalocean_droplet.node.ipv4_address
 }
 
-output "vm_name" {
-  description = "VM instance name"
-  value       = google_compute_instance.node.name
+output "droplet_name" {
+  description = "Назва VM"
+  value       = digitalocean_droplet.node.name
 }
 
 output "bucket_name" {
-  description = "Object storage bucket name"
-  value       = google_storage_bucket.bucket.name
+  description = "Назва bucket"
+  value       = digitalocean_spaces_bucket.bucket.name
 }
 
-output "bucket_url" {
-  description = "Bucket URL"
-  value       = google_storage_bucket.bucket.url
+output "bucket_domain" {
+  description = "Домен bucket"
+  value       = digitalocean_spaces_bucket.bucket.bucket_domain_name
 }
